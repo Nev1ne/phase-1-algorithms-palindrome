5f710479e51a89413a6ef22c8b119187b008096e
Nevine Achola <nevine.achola@student.moringaschool.com>
2023-03-29 15:26:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedStr = word.toLowerCase().split("").reverse().join("");
+  if (reversedStr === word) return true;
+  return false;
 }
 
 /* 
   Add your pseudocode here
 */
-
+-reverse string
+if reversed is equal to word
+   return true
+else 
+   return false
 /*
   Add written explanation of your solution here
 */
+-convert to lowercase
+  -reverse the string
+  -check if it is equal to the string
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
